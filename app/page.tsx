e29fdb318279793c6ded5035979e5570308cfa02
Zhang ShengYan <yan.code@gmail.com>
2025-05-12 09:12:54 +0800

    feat: theme change

--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,21 @@ import { TransactionHistory } from "@/components/transaction-history"
 import { SendToMany } from "@/components/send-to-many"
 import { CloseAccount } from "@/components/close-account"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { ThemeToggle } from "@/components/theme-toggle"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-4 md:p-24 bg-gradient-to-b from-purple-900/10 to-teal-900/10">
+    <main className="flex min-h-screen flex-col items-center  bg-gradient-to-b from-purple-900/10 to-teal-900/10">
+      <div className="flex justify-end w-full p-4 md:p-15">
+        <ThemeToggle />
+      </div>
       <div className="z-10 max-w-5xl w-full flex flex-col items-center">
         <h1 className="text-4xl font-bold mb-8 text-center bg-clip-text text-transparent bg-gradient-to-r from-purple-500 to-teal-400">
-          Solana Wallet Integration
+          Solana Utils
         </h1>
 
         <div className="w-full max-w-md flex flex-col gap-8">
-          <div className="flex justify-center">
+          <div className="flex justify-center items-center gap-4">
             <WalletMultiButton />
           </div>
 
--- components/wallet-multi-button.tsx
+++ components/wallet-multi-button.tsx
@@ -2,7 +2,7 @@
 
 import { useWallet } from "@solana/wallet-adapter-react"
 import { WalletMultiButton as WalletMultiButtonUI } from "@solana/wallet-adapter-react-ui"
-import { useEffect, useState } from "react"
+
 
 export function WalletMultiButton() {
   return <WalletMultiButtonUI className="wallet-adapter-button-custom" />
